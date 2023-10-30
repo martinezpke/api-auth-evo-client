6bcc79b898b838290ad3842e886b38a429a2778b
martinezpke <martinezpke26@gmail.com>
2023-10-29 20:37:18 -0500

    conectando el route con el controlador

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const { login } = require('../controllers/controllerLogin')
 
-router.get('/', (req, res) => {
-    res.send('hello world')
-})
+router.get('/', login)
 
 module.exports = router;
